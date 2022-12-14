0ff4fb69b47c165136facb6f442f76e7bca1be2c
bibitto <bibitto.crypto@gmail.com>
2022-12-14 11:46:20 +0900

    refactor: sortMenu component

--- src/components/common/sort-menu/sort-menu.tsx
+++ src/components/common/sort-menu/sort-menu.tsx
@@ -1,18 +1,19 @@
 import { ChevronDownIcon, CheckIcon } from '@chakra-ui/icons';
 import { Button, Menu, MenuButton, Text, MenuList, MenuItem } from '@chakra-ui/react';
+import { useState } from 'react';
 
 type stringObject = {
   [key: string]: string;
 };
 
 type Props = {
-  state: string;
-  changeState: (value: string) => void;
-  selectOptions: stringObject;
+  name: string;
+  changeSortType: (value: string) => void;
+  typeOptions: stringObject;
 };
 
-export const SortMenu: React.FC<Props> = ({ state, changeState, selectOptions }) => {
-  const keys = Object.keys(selectOptions);
+export const SortMenu: React.FC<Props> = ({ name, changeSortType, typeOptions }) => {
+  const [selectedType, setSelectedType] = useState<string>('all');
   return (
     <Menu>
       <MenuButton
@@ -23,20 +24,23 @@ export const SortMenu: React.FC<Props> = ({ state, changeState, selectOptions })
         _hover={{ bg: 'white' }}
         _active={{ bg: 'white' }}
       >
-        Media
+        {name}
       </MenuButton>
       <MenuList w={180} p={4} border='solid 1px black' borderRadius='0px' _active={{ bg: 'white' }}>
-        {keys.map((key) => {
+        {Object.keys(typeOptions).map((key) => {
           return (
             <MenuItem
               key={key}
-              pl={state === selectOptions[key] ? 0 : 6}
+              pl={selectedType === typeOptions[key] ? 0 : 6}
               gap={2}
               _hover={{ bg: 'white' }}
               _focus={{ bg: 'white' }}
-              onClick={() => changeState(selectOptions[key])}
+              onClick={() => {
+                changeSortType(typeOptions[key]);
+                setSelectedType(typeOptions[key]);
+              }}
             >
-              {state === selectOptions[key] && <CheckIcon fontSize={16} />}
+              {selectedType === typeOptions[key] && <CheckIcon fontSize={16} />}
               <Text fontWeight='bold'>{key}</Text>
             </MenuItem>
           );
