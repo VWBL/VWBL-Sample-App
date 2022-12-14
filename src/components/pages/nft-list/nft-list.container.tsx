4967a82e45511839c3760edc5a201507628ebbd1
bibitto <bibitto.crypto@gmail.com>
2022-12-14 11:49:43 +0900

    refactor: NFTListComponent

--- src/components/pages/nft-list/nft-list.container.tsx
+++ src/components/pages/nft-list/nft-list.container.tsx
@@ -1,18 +1,37 @@
 import { useState, useEffect, useCallback } from 'react';
-
 import { Metadata } from 'vwbl-sdk';
 import { NFTListComponent } from './nft-list';
-import { fetchAllTokens } from '../../../utils';
+import { fetchAllTokens, MEDIA_TYPE } from '../../../utils';
 
 export const NFTList = () => {
   const [nfts, setNfts] = useState<(Metadata | undefined)[]>([]);
+  const [sortedNFTs, setSortedNFTs] = useState<(Metadata | undefined)[]>([]);
   const [isOpenModal, setIsOpenModal] = useState<boolean>(false);
+  const [sortType, setSortType] = useState<string>('all');
 
   const loadNFTs = useCallback(async () => {
     const items = await fetchAllTokens();
     if (!items) return;
     setNfts(items.reverse());
   }, []);
+
+  const sortNFTs = (nfts: (Metadata | undefined)[], sortType: string) => {
+    if (sortType === MEDIA_TYPE.All) {
+      return nfts;
+    } else {
+      const sortedNFTs: (Metadata | undefined)[] = [];
+      for (const nft of nfts) {
+        if (sortType === nft?.mimeType.split('/')[0]) sortedNFTs.push(nft);
+      }
+      return sortedNFTs;
+    }
+  };
+
+  const changeSortType = (newType: string) => setSortType(newType);
+
+  useEffect(() => {
+    setSortedNFTs(sortNFTs(nfts, sortType));
+  }, [sortType, nfts]);
 
   useEffect(() => {
     try {
@@ -23,5 +42,12 @@ export const NFTList = () => {
     }
   }, [loadNFTs]);
 
-  return <NFTListComponent nfts={nfts} isOpenModal={isOpenModal} onCloseModal={() => setIsOpenModal(false)} />;
+  return (
+    <NFTListComponent
+      nfts={sortedNFTs}
+      isOpenModal={isOpenModal}
+      onCloseModal={() => setIsOpenModal(false)}
+      changeSortType={changeSortType}
+    />
+  );
 };
--- src/components/pages/nft-list/nft-list.tsx
+++ src/components/pages/nft-list/nft-list.tsx
@@ -1,17 +1,19 @@
-import { Container } from '@chakra-ui/react';
+import { Box, Container } from '@chakra-ui/react';
 import { Metadata } from 'vwbl-sdk';
-
 import { ItemList } from '../../common/item-list';
 import { CustomLoading } from '../../common/custom-loading';
 import { NotificationModal, notifications } from '../../common/notification-modal';
+import { SortMenu } from '../../common/sort-menu';
+import { MEDIA_TYPE } from '../../../utils';
 
 type Props = {
   nfts: (Metadata | undefined)[];
   isOpenModal: boolean;
   onCloseModal: () => void;
+  changeSortType: (arg0: string) => void;
 };
 
-export const NFTListComponent: React.FC<Props> = ({ nfts, isOpenModal, onCloseModal }) => {
+export const NFTListComponent: React.FC<Props> = ({ nfts, isOpenModal, onCloseModal, changeSortType }) => {
   if (!nfts || nfts.length === 0) {
     return <CustomLoading />;
   }
@@ -19,6 +21,9 @@ export const NFTListComponent: React.FC<Props> = ({ nfts, isOpenModal, onCloseMo
   return (
     <Container maxW='container.lg' pt={12}>
       <NotificationModal isOpen={isOpenModal} onClose={onCloseModal} notification={notifications.load_failed} />
+      <Box display='flex' justifyContent='right' px={4}>
+        <SortMenu name='Media' changeSortType={changeSortType} typeOptions={MEDIA_TYPE} />
+      </Box>
       <ItemList nfts={nfts} />
     </Container>
   );
