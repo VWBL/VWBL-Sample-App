3714f1b0253e5fb6074f4730f05c4db4813ede0e
Asuma Yamada <yamadaasuma@gmail.com>
2023-03-30 15:51:34 +0900

    fix: modal component

--- src/components/common/mint-step-modal/mint-step-modal.tsx
+++ src/components/common/mint-step-modal/mint-step-modal.tsx
@@ -1,7 +1,8 @@
 import { Modal, ModalContent, ModalHeader, ModalOverlay, Text, Box, Button, CircularProgress, Flex, Center } from '@chakra-ui/react';
 import { CheckIcon } from '@chakra-ui/icons';
-import Link from 'next/link';
 import { StepStatus } from 'vwbl-sdk';
+import Link from 'next/link';
+import { memo } from 'react';
 
 export type Props = {
   isOpen: boolean;
@@ -18,114 +19,112 @@ export type MintStepProps = {
   handleMintStart: () => void;
 };
 
-export const MintStepModal: React.FC<Props> = ({ isOpen, signature, mintStep, onClose, handleCancelClick, handleMintStart }) => {
+export const MintStepModal: React.FC<Props> = memo(({ isOpen, signature, mintStep, onClose, handleCancelClick, handleMintStart }) => {
   return (
     <Modal isOpen={isOpen} onClose={onClose} isCentered>
       <ModalOverlay />
       <ModalContent p={5} mx={'a'}>
-        {mintStep.includes(StepStatus.SET_KEY) ? (
-          <>
-            <ModalHeader fontSize={'2xl'}>You have minted your VWBL NFT! 🎉</ModalHeader>
-            <Center>
-              <Link href={`/account/`} replace>
-                <Button color='white' bg='black' w={'90%'}>
-                  My page
+        <ModalHeader fontSize={'2xl'}>Follow the steps</ModalHeader>
+
+        <Box my={3}>
+          <Flex alignItems={'center'} mb={6} px={6}>
+            <Box mr={8}>
+              {mintStep.length < 1 || signature ? (
+                <CheckIcon sx={{ width: 30, height: 30 }} color={signature ? 'black' : 'gray.200'} />
+              ) : (
+                <CircularProgress isIndeterminate size='30px' color='black' />
+              )}
+            </Box>
+            <Box>
+              <Text fontSize='xl' as='b'>
+                Sign
+              </Text>
+              <Text>Sign a message to start using VWBL</Text>
+            </Box>
+          </Flex>
+
+          <Flex alignItems={'center'} mb={6} px={6}>
+            <Box mr={8}>
+              {!mintStep.includes(StepStatus.ENCRYPT_DATA) || mintStep.includes(StepStatus.UPLOAD_METADATA) ? (
+                <CheckIcon sx={{ width: 30, height: 30 }} color={mintStep.includes(StepStatus.UPLOAD_METADATA) ? 'black' : 'gray.200'} />
+              ) : (
+                <CircularProgress isIndeterminate size='30px' color='black' />
+              )}
+            </Box>
+            <Box>
+              <Text fontSize='xl' as='b'>
+                Upload
+              </Text>
+              <Text>Uploading of all media assets and metadata to IPFS</Text>
+            </Box>
+          </Flex>
+
+          <Flex alignItems={'center'} mb={6} px={6}>
+            <Box mr={8}>
+              {!mintStep.includes(StepStatus.UPLOAD_METADATA) || mintStep.includes(StepStatus.MINT_TOKEN) ? (
+                <CheckIcon sx={{ width: 30, height: 30 }} color={mintStep.includes(StepStatus.MINT_TOKEN) ? 'black' : 'gray.200'} />
+              ) : (
+                <CircularProgress isIndeterminate size='30px' color='black' />
+              )}
+            </Box>
+            <Box>
+              <Text fontSize='xl' as='b'>
+                Mint
+              </Text>
+              <Text>Send transaction to create your NFT</Text>
+            </Box>
+          </Flex>
+
+          <Flex alignItems={'center'} mb={6} px={6}>
+            <Box mr={8}>
+              {!mintStep.includes(StepStatus.MINT_TOKEN) || mintStep.includes(StepStatus.SET_KEY) ? (
+                <CheckIcon sx={{ width: 30, height: 30 }} color={mintStep.includes(StepStatus.SET_KEY) ? 'black' : 'gray.200'} />
+              ) : (
+                <CircularProgress isIndeterminate size='30px' color='black' />
+              )}
+            </Box>
+            <Box>
+              <Text fontSize='xl' as='b'>
+                Set key
+              </Text>
+              <Text>Save decryption key to VWBL Network</Text>
+            </Box>
+          </Flex>
+          {mintStep.includes(StepStatus.SET_KEY) ? (
+            <>
+              <Center mt={10} mb={6}>
+                <Text fontSize={'xl'} as='b'>
+                  You have minted your VWBL NFT! 🎉
+                </Text>
+              </Center>
+              <Center>
+                <Link href={`/account/`} replace>
+                  <Button color='white' bg='black' w={'90%'}>
+                    My page
+                  </Button>
+                </Link>
+              </Center>
+            </>
+          ) : (
+            <>
+              <Center my={4}>
+                <Button type='submit' color='white' bg='black' w={'90%'} disabled={mintStep.length > 0} onClick={handleMintStart}>
+                  {mintStep.length < 1 && 'Start'}
+                  {mintStep.length > 1 && 'In Progress...'}
+                  {mintStep.includes(StepStatus.SET_KEY) && 'Minted'}
                 </Button>
-              </Link>
-            </Center>
-          </>
-        ) : (
-          <>
-            <ModalHeader fontSize={'2xl'}>Follow the steps</ModalHeader>
+              </Center>
 
-            <MintStep mintStep={mintStep} signature={signature} handleMintStart={handleMintStart} />
-
-            <Center>
-              <Button color='black' bg='white' variant='outline' onClick={handleCancelClick} w={'90%'}>
-                Cancel
-              </Button>
-            </Center>
-          </>
-        )}
+              <Center>
+                <Button color='black' bg='white' variant='outline' onClick={handleCancelClick} w={'90%'}>
+                  Cancel
+                </Button>
+              </Center>
+            </>
+          )}
+        </Box>
       </ModalContent>
     </Modal>
   );
-};
-
-const MintStep: React.FC<MintStepProps> = ({ mintStep, signature, handleMintStart }) => {
-  return (
-    <Box my={3}>
-      <Flex alignItems={'center'} mb={6} px={6}>
-        <Box mr={8}>
-          {mintStep.length < 1 || signature ? (
-            <CheckIcon sx={{ width: 30, height: 30 }} color={signature ? 'black' : 'gray.200'} />
-          ) : (
-            <CircularProgress isIndeterminate size='30px' color='black' />
-          )}
-        </Box>
-        <Box>
-          <Text fontSize='xl' as='b'>
-            Sign
-          </Text>
-          <Text>Sign a message to start using VWBL</Text>
-        </Box>
-      </Flex>
-
-      <Flex alignItems={'center'} mb={6} px={6}>
-        <Box mr={8}>
-          {!mintStep.includes(StepStatus.ENCRYPT_DATA) || mintStep.includes(StepStatus.UPLOAD_METADATA) ? (
-            <CheckIcon sx={{ width: 30, height: 30 }} color={mintStep.includes(StepStatus.UPLOAD_METADATA) ? 'black' : 'gray.200'} />
-          ) : (
-            <CircularProgress isIndeterminate size='30px' color='black' />
-          )}
-        </Box>
-        <Box>
-          <Text fontSize='xl' as='b'>
-            Upload
-          </Text>
-          <Text>Uploading of all media assets and metadata to IPFS</Text>
-        </Box>
-      </Flex>
-
-      <Flex alignItems={'center'} mb={6} px={6}>
-        <Box mr={8}>
-          {!mintStep.includes(StepStatus.UPLOAD_METADATA) || mintStep.includes(StepStatus.MINT_TOKEN) ? (
-            <CheckIcon sx={{ width: 30, height: 30 }} color={mintStep.includes(StepStatus.MINT_TOKEN) ? 'black' : 'gray.200'} />
-          ) : (
-            <CircularProgress isIndeterminate size='30px' color='black' />
-          )}
-        </Box>
-        <Box>
-          <Text fontSize='xl' as='b'>
-            Mint
-          </Text>
-          <Text>Send transaction to create your NFT</Text>
-        </Box>
-      </Flex>
-
-      <Flex alignItems={'center'} mb={6} px={6}>
-        <Box mr={8}>
-          {!mintStep.includes(StepStatus.MINT_TOKEN) || mintStep.includes(StepStatus.SET_KEY) ? (
-            <CheckIcon sx={{ width: 30, height: 30 }} color={mintStep.includes(StepStatus.SET_KEY) ? 'black' : 'gray.200'} />
-          ) : (
-            <CircularProgress isIndeterminate size='30px' color='black' />
-          )}
-        </Box>
-        <Box>
-          <Text fontSize='xl' as='b'>
-            Set key
-          </Text>
-          <Text>Save decryption key to VWBL Network</Text>
-        </Box>
-      </Flex>
-
-      <Center>
-        <Button type='submit' color='white' bg='black' w={'90%'} disabled={mintStep.length > 0} onClick={handleMintStart}>
-          {mintStep.length < 1 && 'Start'}
-          {mintStep.length > 1 && 'In Progress...'}
-          {mintStep.includes(StepStatus.SET_KEY) && 'Minted'}
-        </Button>
-      </Center>
-    </Box>
-  );
-};
+});
+MintStepModal.displayName = 'MintStepModal';
