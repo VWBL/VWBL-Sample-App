b493089f00212389a757d55725b2e6e28c70e5a4
Taki <hashimoto19980924@gmail.com>
2023-09-04 11:51:52 +0900

    feat: query owned nft by alchemy api

--- src/components/pages/account/account.container.tsx
+++ src/components/pages/account/account.container.tsx
@@ -5,10 +5,10 @@ import { VwblContainer } from '../../../container';
 import { ExtendedMetadeta } from 'vwbl-sdk';
 import { switchChain } from '../../../utils';
 import { ethers } from 'ethers';
+import axios from 'axios';
 
 export const Account = () => {
   const [ownedNfts, setOwnedNfts] = useState<ExtendedMetadeta[]>([]);
-  const [mintedNfts, setMintedNfts] = useState<ExtendedMetadeta[]>([]);
   const [isOpenModal, setIsOpenModal] = useState(false);
   const [walletAddress, setWalletAddress] = useState('');
 
@@ -33,10 +33,22 @@ export const Account = () => {
         return;
       }
       try {
-        const ownedItems = await vwblViewer.listOwnedNFTMetadata(userAddress);
-        setOwnedNfts(ownedItems.filter((v) => v).reverse() as ExtendedMetadeta[]);
-        const mintedItems = await vwblViewer.listMintedNFTMetadata(userAddress);
-        setMintedNfts(mintedItems.filter((v) => v).reverse() as ExtendedMetadeta[]);
+        const query = `${process.env.NEXT_PUBLIC_ALCHEMY_NFT_API}/getNFTs?owner=${walletAddress}`;
+        const result = await axios.get(query);
+        const ownedItems = result.data.ownedNfts.filter((v: any) => {
+          return typeof v.metadata.encrypted_data !== "undefined"
+        }).map((v: any) => {
+          return {
+            id: Number(v.id.tokenId),
+            name: v.metadata.name,
+            description: v.metadata.description,
+            image: v.metadata.image,
+            mimeType: v.metadata.mime_type,
+            encryptLogic: v.metadata.encrypt_logic,
+            address: v.contract.address,
+          } as ExtendedMetadeta
+        }).reverse();
+        setOwnedNfts(ownedItems);
       } catch (err) {
         setIsOpenModal(true);
         console.log(err);
@@ -48,7 +60,6 @@ export const Account = () => {
   return (
     <AccountComponent
       ownedNfts={ownedNfts}
-      mintedNfts={mintedNfts}
       walletAddress={walletAddress}
       isOpenModal={isOpenModal}
       onCloseModal={() => setIsOpenModal(false)}
--- src/components/pages/account/account.tsx
+++ src/components/pages/account/account.tsx
@@ -6,23 +6,12 @@ import { NotificationModal, notifications } from '../../common/notification-moda
 
 type Props = {
   ownedNfts: ExtendedMetadeta[];
-  mintedNfts: ExtendedMetadeta[];
   walletAddress: string;
   isOpenModal: boolean;
   onCloseModal: () => void;
 };
 
-export const AccountComponent: React.FC<Props> = ({ ownedNfts, mintedNfts, walletAddress, isOpenModal, onCloseModal }) => {
-  const tabOptions = [
-    { name: 'Owned', length: ownedNfts.length },
-    { name: 'Created', length: mintedNfts.length },
-  ];
-  const [tabIndex, setTabIndex] = useState(0);
-
-  const handleTabsChange = (index: number) => {
-    setTabIndex(index);
-  };
-
+export const AccountComponent: React.FC<Props> = ({ ownedNfts, walletAddress, isOpenModal, onCloseModal }) => {
   return (
     <Box pt={10}>
       <NotificationModal isOpen={isOpenModal} onClose={onCloseModal} notification={notifications.load_failed} />
@@ -42,28 +31,7 @@ export const AccountComponent: React.FC<Props> = ({ ownedNfts, mintedNfts, walle
         </Stack>
       </Container>
 
-      <Tabs size='md' index={tabIndex} onChange={handleTabsChange} colorScheme='black' variant='line' align='center'>
-        <TabList justifyContent={'center'}>
-          {tabOptions.map((tab, i) => (
-            <Tab px={4} key={i} fontWeight='bold' position='relative'>
-              <Text px={1}>{`${tab.name}`}</Text>
-              <Badge bg='white' color='gray.400' size='md' position='absolute' top={0} right={0} p={0}>
-                {tab.length}
-              </Badge>
-            </Tab>
-          ))}
-        </TabList>
-
-        <TabPanels mt={6} maxW='container.lg'>
-          <TabPanel>
-            <ItemList nfts={ownedNfts} />
-          </TabPanel>
-
-          <TabPanel>
-            <ItemList nfts={mintedNfts} />
-          </TabPanel>
-        </TabPanels>
-      </Tabs>
+      <ItemList nfts={ownedNfts} />
     </Box>
   );
 };
