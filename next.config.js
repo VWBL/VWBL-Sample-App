86d28ff0172bbfe4f22322fa1a689d526b572960
Huzitatuguharu <sgrko.4751171@gmail.com>
2023-12-07 14:22:19 +0900

    fix: package.json

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'export',
   webpack: (config, { isServer }) => {
     if (!isServer) {
       config.resolve.fallback.fs = false;
