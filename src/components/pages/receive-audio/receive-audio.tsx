3d1aa59353ebb75edf956a5f1fe06efde8cd2dbf
Nagai <wakoto3@gmail.com>
2024-03-29 16:57:14 +0900

    fix audio page

--- src/components/pages/receive-audio/receive-audio.tsx
+++ src/components/pages/receive-audio/receive-audio.tsx
@@ -1,4 +1,4 @@
-import { Heading, Box, Container, Text } from '@chakra-ui/react';
+import { Heading, Box, Container, Text, Link } from '@chakra-ui/react';
 import { sampleAudioNFT } from './receive-audio.container';
 
 import { Button } from '../../common/button';
@@ -13,11 +13,11 @@ export const ReceiveAudioNFTComponent: React.FC<Props> = ({ onSubmit, isLoading 
   return (
     <Container maxW='container.md' my={12} centerContent>
       <Box w='100%' maxW={480} mb='20'>
-        <Heading as='h2' mb={12} w='100%'>
+        <Heading as='h2' mb={12} w='100%' textAlign='center'>
           Music NFT を無料で受け取る
         </Heading>
 
-        <Text>
+        <Text mt={6} textAlign='center'>
           暗号屋の大人気曲「ブロックチェーンパーティー」の
           <br />
           テーマソングの音源をNFTでお届けします。
@@ -49,6 +49,25 @@ export const ReceiveAudioNFTComponent: React.FC<Props> = ({ onSubmit, isLoading 
             />
           </Box>
         </form>
+
+        <Text mt={6} textAlign='center'>
+          「Music NFT を無料で受け取る」をクリックすると、
+          <br />
+          NFT受取サイトへ移動します。
+          <br />
+          受取にはMetaMask（メタマスク）のインストールが必要です。
+          <br />
+          ※ これも本当のことです。
+          <br />
+          <br />
+          <Link
+            color='blue.600'
+            href='https://support.metamask.io/hc/ja/articles/360015489531-MetaMask%E3%82%92%E5%A7%8B%E3%82%81%E3%82%8B'
+            isExternal
+          >
+            MetaMaskとは？
+          </Link>
+        </Text>
       </Box>
     </Container>
   );
