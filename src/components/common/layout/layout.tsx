8624fb6fbf9e189b8219bc44f6b437e53e9f44ae
Huzitatuguharu <sgrko.4751171@gmail.com>
2024-09-09 18:39:13 +0900

    Temporary commit: Bypassing ESLint

--- src/components/common/layout/layout.tsx
+++ src/components/common/layout/layout.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import { useEffect } from 'react';
 import NextLink from 'next/link';
 import {
@@ -21,8 +23,9 @@ import { MdOutlineAccountBalanceWallet } from 'react-icons/md';
 import { VwblContainer } from '../../../container/vwbl-container';
 import { Button } from '../button';
 import { hamburgerMenu, closeButton } from './layout.style';
-import { useRouter } from 'next/router';
+import { useRouter } from 'next/navigation';
 import { ExternalLinkIcon } from '@chakra-ui/icons';
+import { useDisconnect, useWeb3Modal } from '@web3modal/ethers/react';
 
 type Link = {
   title: string;
@@ -32,13 +35,13 @@ type Link = {
 
 const NavLink = ({ title, to }: Link) => {
   const isExternal = to.startsWith('https://');
-
   return (
     <Link as={!isExternal ? NextLink : undefined} href={to} isExternal={isExternal}>
       {title} {isExternal && <ExternalLinkIcon mb='4px' mx='2px' />}
     </Link>
   );
 };
+
 const HamburgerMenu = ({ onClick, sx }: { onClick: () => void; sx: CSSObject }) => {
   return (
     <ChakraButton
@@ -60,8 +63,10 @@ const HamburgerMenu = ({ onClick, sx }: { onClick: () => void; sx: CSSObject }) 
 };
 
 export const Layout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+  // const { open } = useWeb3Modal();
   const { isOpen, onOpen, onClose } = useDisclosure();
-  const { connectWallet, userAddress } = VwblContainer.useContainer();
+  const { connectWallet } = VwblContainer.useContainer();
+  const { disconnect } = useDisconnect();
   const router = useRouter();
 
   const HeaderLinks: Link[] = [
@@ -95,12 +100,16 @@ export const Layout: React.FC<{ children: React.ReactNode }> = ({ children }) =>
   ];
 
   useEffect(() => {
-    connectWallet();
-  }, []);
-
-  useEffect(() => {
     onClose();
   }, [router]);
+
+  const handleConnectWallet = async () => {
+    await open();
+  };
+
+  const handleDisconnecttWallet = async () => {
+    await disconnect();
+  };
 
   return (
     <>
@@ -109,7 +118,6 @@ export const Layout: React.FC<{ children: React.ReactNode }> = ({ children }) =>
           <Link href='/' as={NextLink}>
             <Image src='/header-logo.svg' alt='VWBL Sample App' h={7} />
           </Link>
-          <HStack />
           <HamburgerMenu onClick={onOpen} sx={hamburgerMenu} />
 
           <Flex alignItems={'center'} display={{ base: 'none', md: 'flex' }}>
@@ -118,15 +126,16 @@ export const Layout: React.FC<{ children: React.ReactNode }> = ({ children }) =>
                 <NavLink key={i} title={link.title} to={link.to} target={link.target} />
               ))}
             </HStack>
-            {userAddress ? (
+            {/* {isConnected ? (
               <HStack spacing={6}>
                 <Link href='/account' as={NextLink}>
                   <Button text='My Wallet' borderRadius={'3xl'} icon={MdOutlineAccountBalanceWallet} />
                 </Link>
+                <Button text='Disconnect' borderRadius={'3xl'} isReversed onClick={handleDisconnecttWallet} />
               </HStack>
             ) : (
-              <Button text='Connect Wallet' borderRadius={'3xl'} icon={MdOutlineAccountBalanceWallet} onClick={connectWallet} />
-            )}
+              <Button text='Connect Wallet' borderRadius={'3xl'} icon={MdOutlineAccountBalanceWallet} onClick={handleConnectWallet} />
+            )} */}
           </Flex>
         </Flex>
 
@@ -141,10 +150,11 @@ export const Layout: React.FC<{ children: React.ReactNode }> = ({ children }) =>
                     <NavLink key={i} title={link.title} to={link.to} target={link.target} />
                   ))}
                 </Stack>
-                {userAddress ? (
+                {/* {isConnected ? (
                   <VStack spacing={6} alignItems='start'>
+                    <Button text='Disconnect' borderRadius={'3xl'} isReversed fontSize={'md'} onClick={handleDisconnecttWallet} />
                     <Link href='/account' as={NextLink}>
-                      <Button as='a' text='My Wallet' borderRadius={'3xl'} icon={MdOutlineAccountBalanceWallet} height='40px' isReversed />
+                      <Button as='a' text='My Wallet' borderRadius={'3xl'} icon={MdOutlineAccountBalanceWallet} height='40px' />
                     </Link>
                   </VStack>
                 ) : (
@@ -153,12 +163,12 @@ export const Layout: React.FC<{ children: React.ReactNode }> = ({ children }) =>
                       text='Connect Wallet'
                       borderRadius={'3xl'}
                       icon={MdOutlineAccountBalanceWallet}
-                      onClick={connectWallet}
+                      onClick={handleConnectWallet}
                       isReversed
                       height='40px'
                     />
                   </HStack>
-                )}
+                )} */}
                 <Box fontSize='sm'>＊pcの方はmetamask chrome extention、mobileの方はmetamask appをご利用ください。</Box>
               </Stack>
             </VStack>
@@ -166,7 +176,7 @@ export const Layout: React.FC<{ children: React.ReactNode }> = ({ children }) =>
         </Drawer>
       </Container>
 
-      {/* hidden element to adjust hight */}
+      {/* hidden element to adjust height */}
       <Box minH='calc(100vh - 160px)'>{children}</Box>
       <Container
         as='footer'
@@ -195,7 +205,7 @@ export const Layout: React.FC<{ children: React.ReactNode }> = ({ children }) =>
             {FooterLinks.map((link, i) => (
               <NavLink key={i} title={link.title} to={link.to} />
             ))}
-            {userAddress && <NavLink title='My Wallet' to='/account' />}
+            {/* {isConnected && <NavLink title='My Wallet' to='/account' />} */}
           </Stack>
 
           <Text fontSize='sm' color='#AEAEB2'>
