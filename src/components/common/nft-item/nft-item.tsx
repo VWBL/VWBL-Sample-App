7a338a24ee6939d430f89f6699e8431835594883
Tatsuki <hashimoto19980924@gmail.com>
2025-04-11 14:04:31 +0900

    fix: warin

--- src/components/common/nft-item/nft-item.tsx
+++ src/components/common/nft-item/nft-item.tsx
@@ -9,27 +9,27 @@ type Props = {
 
 export const NFTItemComponent: React.FC<Props> = ({ nft, disabled }) => {
   return (
-    <Link href={`/assets/${nft.address}/${nft.id}`} passHref>
-      <VStack
-        bg={useColorModeValue('white', 'gray.800')}
-        width={300}
-        pt={8}
-        pb={5}
-        gap={6}
-        style={{ pointerEvents: disabled ? 'none' : undefined }}
-      >
+    <VStack
+      bg={useColorModeValue('white', 'gray.800')}
+      width={300}
+      pt={8}
+      pb={5}
+      gap={6}
+      style={{ pointerEvents: disabled ? 'none' : undefined }}
+    >
+      <Link href={`/assets/${nft.address}/${nft.id}`} passHref>
         <Box padding={'40px 30px'} bgColor='black' minHeight={426} width='100%' display='flex' alignItems='center' justifyContent='center'>
           <Image src={nft.image} alt={`Picture of ${nft.name}`} />
         </Box>
-        <VStack align='start' w='100%'>
-          <Text fontSize='lg' fontWeight='bold' justifyContent='flex-start' textAlign='left' w='100%'>
-            {nft.name}
-          </Text>
-          <Text fontSize='sm' noOfLines={3} textAlign='left'>
-            {nft.description}
-          </Text>
-        </VStack>
+      </Link>
+      <VStack align='start' w='100%'>
+        <Text fontSize='lg' fontWeight='bold' justifyContent='flex-start' textAlign='left' w='100%'>
+          {nft.name}
+        </Text>
+        <Text fontSize='sm' noOfLines={3} textAlign='left'>
+          {nft.description}
+        </Text>
       </VStack>
-    </Link>
+    </VStack>
   );
 };
