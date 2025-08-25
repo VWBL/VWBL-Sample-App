50da0eea2010d638868ac59c6a3c6c8539d3ddff
yuta-mine <jamyukirom@gmail.com>
2025-08-25 15:22:56 +0900

    ウォレット選択

--- src/components/common/receive-nft/wallet-info.tsx
+++ src/components/common/receive-nft/wallet-info.tsx
@@ -1,4 +1,4 @@
-import { Text, Link, VStack } from '@chakra-ui/react';
+import { Text, Link, VStack, HStack } from '@chakra-ui/react';
 import { ExternalLinkIcon } from '@chakra-ui/icons';
 
 export const WalletInfo: React.FC = () => {
@@ -7,17 +7,24 @@ export const WalletInfo: React.FC = () => {
       <Text textAlign='center'>
         NFTを受け取るには、仮想通貨用のウォレットが必要です。
         <br />
-        VWBL Demoでは
-        <Link color='blue.600' href='https://metamask.io/download/' isExternal>
-          MetaMask(メタマスク)
-          <ExternalLinkIcon mx='2px' mb='2px' />
-        </Link>
-        をご利用ください。
       </Text>
+      <VStack spacing={2}>
+        <HStack spacing={4} justify="center">
+          <Link color='blue.600' href='https://metamask.io/download/' isExternal>
+            MetaMask(メタマスク)
+            <ExternalLinkIcon mx='2px' mb='2px' />
+          </Link>
+          <Text>または</Text>
+          <Link color='blue.600' href='https://walletconnect.com/explorer' isExternal>
+            WalletConnect対応ウォレット
+            <ExternalLinkIcon mx='2px' mb='2px' />
+          </Link>
+        </HStack>
+      </VStack>
       <Text textAlign='center'>
-        PCの方：Chromeブラウザ
+        <strong>PCの方：</strong>Chromeブラウザ
         <br />
-        Mobileの方：Metamask app内ブラウザ
+        <strong>Mobileの方：</strong>Metamask app内ブラウザ または WalletConnect対応アプリ
         <br />
         でVWBL Demoを開いてください。
       </Text>
